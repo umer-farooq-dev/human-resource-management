36b5210c95dc3c6d49f937917b467f9ec2959019
umer-farooq-dev <ufkeyfans786@gmail.com>
2022-08-29 22:26:52 +0500

    Templating

--- hrm_admin/views.py
+++ hrm_admin/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def BusinessStrategy(request):
+    return render(request=request, template_name="busniess.html")
+    
 def home(request):
     return render(request=request, template_name="home.html")
 
 
 def login(request):
     return render(request=request, template_name="login.html")
+
+
+
+def NewsFeed(request):
+    return render(request=request, template_name="newsfeed.html")
+
+
+def Onedrive(request):
+    return render(request=request, template_name="onedrive.html")
+
+
+def Organization(request):
+    return render(request=request, template_name="organization.html")
+
+    
--- human_resource_management/urls.py
+++ human_resource_management/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.login, name="login"),
                   path('home/', views.home, name="home"),
+                  path('newsfeed/', views.NewsFeed, name="newsfeed"),
+                  path('onedrive/', views.Onedrive, name="onedrive"),
+                  path('organization/', views.Organization, name="organization"),
+                  path('business/', views.BusinessStrategy, name="business"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
                                                                                          document_root=settings.STATIC_ROOT)
