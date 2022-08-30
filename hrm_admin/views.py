77c40c17234980f3608baf6375448bd7812ad103
umer-farooq-dev <ufkeyfans786@gmail.com>
2022-08-30 23:06:43 +0500

    Add templates

--- hrm_admin/views.py
+++ hrm_admin/views.py
@@ -4,15 +4,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def BusinessStrategy(request):
-    return render(request=request, template_name="busniess.html")
-    
+    return render(request=request, template_name="business.html")
+
+
 def home(request):
     return render(request=request, template_name="home.html")
 
 
 def login(request):
     return render(request=request, template_name="login.html")
-
 
 
 def NewsFeed(request):
@@ -26,4 +26,18 @@ def Onedrive(request):
 def Organization(request):
     return render(request=request, template_name="organization.html")
 
-    
+
+def human_resources(request):
+    return render(request=request, template_name="human_resources.html")
+
+
+def tech_service(request):
+    return render(request=request, template_name="tech_service.html")
+
+
+def ethics_security(request):
+    return render(request=request, template_name="ethics_security.html")
+
+
+def leadership(request):
+    return render(request=request, template_name="leadership.html")
